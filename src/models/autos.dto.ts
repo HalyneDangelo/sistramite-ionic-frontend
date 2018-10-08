0c74f6287ff62773a9e4815161b1d1b4583a3138
Halyne Ribeiro <halyne.dangelo@gmail.com>
2018-10-08 13:18:22 -0300

    Listando categorias

---
+++ src/models/autos.dto.ts
@@ -0,0 +1,5 @@
+export interface AutoDTO {
+    id : string;
+    numero: string;
+    empresa : string;
+}
--- src/pages/autos/autos.ts
+++ src/pages/autos/autos.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { AutoService } from '../../services/domain/autos.service';
 
 /**
  * Generated class for the AutosPage page.
@@ -15,11 +16,20 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class AutosPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    public autoService: AutoService) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad AutosPage');
+    this.autoService.findAll()
+      .subscribe(response => {
+        console.log(response);
+      },
+      error => {
+        console.log(error);
+      });
   }
 
 }
