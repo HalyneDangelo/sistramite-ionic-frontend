0b513d2de1ce3eb7f0d0b52986329720b73a12bd
Halyne Ribeiro <halyne.dangelo@gmail.com>
2019-01-18 12:13:04 -0300

    Tratando erros 403

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -28,7 +28,14 @@ export class ProfilePage {
         this.usuario = response;
         //buscar imagem
       },
-      error => {});
+      error => {
+        if (error.status == 403) {
+          this.navCtrl.setRoot('HomePage');
+        }
+      });
+    }
+    else {
+      this.navCtrl.setRoot('HomePage');
     }
   }
 
